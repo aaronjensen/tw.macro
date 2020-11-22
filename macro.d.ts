c0dbdab175d54128bb42128d1db23dd8e0141205
Aaron Jensen <aaronjensen@gmail.com>
2020-11-21 18:31:06 -0600

    Types

---
+++ macro.d.ts
@@ -0,0 +1,20 @@
+declare const tw: (
+  strings: Readonly<TemplateStringsArray>,
+  ...values: readonly string[]
+) => string
+
+export default tw
+
+declare module 'react' {
+  interface DOMAttributes<T> {
+    tw?: string
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      tw?: string
+    }
+  }
+}
